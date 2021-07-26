dec4c2afd5b94317829e92af4f27925a6aad6e83
Ety Dey <Ety.Dey@albertahealthservices.ca>
2021-07-26 15:08:42 -0600

    updated colors

--- app.js
+++ app.js
@@ -313,7 +313,7 @@ const d3Tableau = () => {
 			if (node.Case_Age >= 0 && node.Case_Age <= 10) color = "#f380f7";
 			if (node.Case_Age >= 11 && node.Case_Age <= 20) color = "#371ce0";
 			if (node.Case_Age >= 21 && node.Case_Age <= 30) color = "#A52A2A";
-			if (node.Case_Age >= 31 && node.Case_Age <= 40)color = "#a75401";
+			if (node.Case_Age >= 31 && node.Case_Age <= 40)color = "#f07d30";
 			if (node.Case_Age >= 41 && node.Case_Age <= 50) color = "#039243";
 			if (node.Case_Age >= 51 && node.Case_Age <= 60) color = "lightblue";
 			if (node.Case_Age >= 61 && node.Case_Age <= 70) color = "yellow";
